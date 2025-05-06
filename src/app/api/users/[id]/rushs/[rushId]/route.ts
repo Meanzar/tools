469772a3cf899d0550e0aa24733a93f513df820e
meanzar <116259100+Meanzar@users.noreply.github.com>
2025-05-06 10:21:54 +0200

    modify get logic

--- src/app/api/users/[id]/rushs/[rushId]/route.ts
+++ src/app/api/users/[id]/rushs/[rushId]/route.ts
@@ -4,13 +4,12 @@ import { ObjectId } from "mongodb";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string; rushId: string } }
+  context: { params: { id: string; rushId: string } }
 ) {
   const client = await clientPromise;
   const db = client.db("tools");
 
-  const userId = params.id;
-  const rushId = params.rushId;
+  const {id: userId, rushId } = context.params
 
   if (!ObjectId.isValid(rushId) || !ObjectId.isValid(userId)) {
     return new Response(JSON.stringify({ error: "Invalid ID" }), { status: 400 });
