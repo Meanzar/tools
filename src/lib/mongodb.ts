20f428e86e3fec6f6962876cbe5882b112aa0e48
meanzar <116259100+Meanzar@users.noreply.github.com>
2025-05-23 10:31:27 +0200

    handle env

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -2,7 +2,7 @@
 /* eslint-disable no-var */
 import { MongoClient } from "mongodb"
 
-const uri = process.env.NEXT_PUBLIC_MONGODB_URI
+const uri = process.env.MONGODB_URI
 if (!uri) {
   throw new Error("Missing NEXT_PUBLIC_MONGODB_URI environment variable")
 }
